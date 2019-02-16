995688ad719a70c1c8d81bcd8a5dd26fad4b2cb7
gyugie <mugypleci@gmail.com>
2019-02-16 10:38:55 +0700

    added folder db and moongose.js file, added folder models and todo.js user.js

---
+++ server/server.js
@@ -0,0 +1,34 @@
+let express		= require('express');
+let bodyParser	= require('body-parser');
+
+var {mongoose} 	= require('./db/mongoose.js');
+var {Todo}		= require('./models/todo');
+var {user}		= require('./models/user');
+
+var app 		= express();
+var port		= 3000; 
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+	var todo = new Todo({
+		text : req.body.text
+	});
+
+	todo.save().then((doc) =>{
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+
+});
+
+
+
+
+app.listen(port, () =>{
+	console.log(`Started on port ${port}`);
+})
+
+
+	
