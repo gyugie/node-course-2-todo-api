a96d9508d8e790815711de4dd1173f9f306ef630
gyugie <mugypleci@gmail.com>
2019-02-16 16:36:37 +0700

    added GET todos routes

--- server/server.js
+++ server/server.js
@@ -25,7 +25,13 @@ app.post('/todos', (req,res) => {
 
 });
 
-
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(todos);
+	});
+});
 
 
 app.listen(port, () =>{
